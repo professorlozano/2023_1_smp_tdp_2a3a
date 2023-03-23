bb66190e7f5f3a2d60dfffbea6534114b1eccd66
Professor Lozano <prof-luiz@outlook.com>
2023-03-23 08:51:34 -0300

    criação do exemplo 15

---
+++ aula06/exemplo15.js
@@ -0,0 +1,18 @@
+class Livro{
+    constructor(pNome, pPreco){
+        this.nome = pNome
+        this.preco = pPreco
+    }
+
+    get Nome(){return this.nome}
+    set Nome(pNome) {this.nome = pNome}
+
+    get Preco(){return this.preco}
+    set Preco(pPreco) {this.preco = pPreco}
+}
+
+var obj_livro1 = new Livro("Use a cabeça Java",200)
+console.log("O livro "+ obj_livro1.nome + " custa " + 
+obj_livro1.preco + " reais")
+
+
