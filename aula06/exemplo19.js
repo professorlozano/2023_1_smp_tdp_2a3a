df205da460f9ecb9beb0b0fa914d9f4e1e8bb40f
Professor Lozano <prof-luiz@outlook.com>
2023-03-23 09:44:07 -0300

    exemplo 19

---
+++ aula06/exemplo19.js
@@ -0,0 +1,27 @@
+class Livro{
+    constructor(pNome, pPreco){
+        this.nome = pNome
+        this.preco = pPreco
+    }
+
+    get Nome(){return this.nome}
+    set Nome(pNome) {this.nome = pNome}
+
+    get Preco(){return this.preco}
+    set Preco(pPreco) {this.preco = pPreco}
+
+    calcularDesconto(pDesconto){
+        this.preco = this.preco - ((this.preco * pDesconto) / 100)
+    }
+}
+
+var obj_livro1 = new Livro("Use a cabeça Java",200)
+var obj_livro2 = new Livro("Use a cabeça C#",170)
+
+var lista = []
+
+lista.push(obj_livro1)
+lista.push(obj_livro2)
+
+//console.log(lista[0])
+console.log(lista)
