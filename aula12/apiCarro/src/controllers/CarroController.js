8037e0c70b5ba6fab0e96088129b79c38377cb1f
Professor Lozano <prof-luiz@outlook.com>
2023-05-18 09:06:17 -0300

    aula 12, rota de buscar um carro

---
+++ aula12/apiCarro/src/controllers/CarroController.js
@@ -0,0 +1,31 @@
+const CarroService = require('../services/CarroService');
+
+module.exports = {
+
+    buscarTodos : async (req, res) => {
+        let json = {error: '', result:[]};
+
+        let carros = await CarroService.buscarTodos();
+
+        for(let i in carros){
+            json.result.push({
+                codigo: carros[i].codigo,
+                descricao: carros[i].modelo
+            });
+        }
+        res.json(json);
+    },
+
+    buscarUm : async (req, res) => {
+        let json = {error: '', result:[]};
+
+        let codigo = req.params.codigo;
+        let carro = await CarroService.buscarUm(codigo);
+
+        if(carro){
+            json.result = carro;
+        }
+
+        res.json(json);
+    }
+}
